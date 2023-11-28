32c5089d616bcd42b87a31357811b96e6e250096
Laura <150727324+WeisssLaura@users.noreply.github.com>
2023-11-28 17:06:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,173 +1,184 @@
- /*console.log(prompt("Qual é o seu nome?")); - "imprime" -
+/* // this just a js file 
 
-var variavel = 100; - muda -
-const variavelConstante = 200; - constante -
+let condicao1 = true;
+if (condicao1){
+    console.log("Entrei no if 1"); /*condição true, o bloco é executado - 
+}
 
-variavel = 400;
-variavelConstante = 600;
+let condicao2 = false;
+if (condicao2){
+    console.log("Entrei no if 2") /*condição false, o bloco não é executado - 
+}
 
-const corDeFundo = #fefefe */
+var num1 = 2;
+var num2 = 1;
+if ( num1 === num2 ){
+ console.log("são iguais");
+}else{
+    if(num1 > num2){
+        console.log("são diferentes e num1 é maior");
+    }else{
+        console.log("são diferentes e num2 é maior");
+    }
+    
+} */
 
-/*const nome = "Laura";
-const idade = 18;
+/* var paisDeOrigem = "brasil"
 
-console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos");*/
-
-/*Numbers - representa numero*/
-/*const idade = 23;
-const altura = 1.79;
-const temperatura = -20;
-
-/*Strings - representa texto
-const nome = "Yuzo";
-var idade = "23";
-
-var souUmBoolean = true;
-console.log(souUmBoolean);
-souUmBoolean = false;
-console.log(souUmBoolean);*/
-
-/* const primeiroNome = "Laura";
-const sobrenome = "Lange";
-const idade = 19;
-const souEstudante = "true";
-
-console.log("nome:", primeiroNome, " Sobrenome:", sobrenome, " idade:", idade, " anos", " sou estudante?", souEstudante,);
-
-console.log("nome: " + primeiroNome + " Sobrenome: " + sobrenome + " idade: " + idade + " anos" + " é estudante?: "+ souEstudante);
-
-var todoTexto = "nome: " + primeiroNome + "  Sobrenome: " + sobrenome + "  idade: " + idade + " anos" + "  é estudante?: "+ souEstudante;
-console.log(todoTexto);
- 
-
-console.log(typeof(primeiroNome));
-console.log(typeof(sobrenome));
-console.log(typeof(idade));
-console.log(typeof(souEstudante)); */
-
-/* - Numero pra texto -
-const idadeNumero = 23;
-const idadeTexto = idadeNumero.toString()
-
-console.log(typeof idadeNumero);
-console.log(typeof idadeTexto);*/
+switch (paisDeOrigem) {
+    case "brasil":
+        console.log("Brasileiro");
+        break;
+        case "EUA":
+        console.log("Americano");
+        break;
 
 
-/* /* - Texto pra Numero - 
-const idadeTexto = "23";
-const idadeNumero = Number(idadeTexto);
-
-console.log(typeof idadeTexto);
-console.log(typeof idadeNumero) */
-
-/* - matematica -
- var n1 = 10;
-var n2 = 5;
-var n3 = 3;
-
-var soma, subtracao, divisao, multiplicacao;
-
-soma = n1 + n2; 
-subtracao = n1 - n2;
-divisao = n1 / n2;
-multiplicacao = n1 * n2;
-
-console.log(soma);
-console.log(subtracao);
-console.log(divisao);
-console.log(multiplicacao); */
-
-/* dividido por 2 = 0 par 
-var numero = 6 
-console.log(numero % 2); */
-
-/* /* comparador - 
-=== igual
-!== diferente 
->= > 
-<= < 
-|| - OU
-&& - E 
-! - negação */
-
-/* var n1 = 2;
-var n2 = 564;
-var n3 = 2;
-var n4 = 3; 
-
-/* console.log(n1 === n2);
-console.log(n1 === n3); 
-
-console.log(n1 !== n2);
-console.log(n1 !== n3);  
-
-console.log(n1 > n3); 
-console.log(n1 >= n3); 
-
-console.log(n1 === n2 && n1 === n3);
-console.log(n1 === n2 || n1 === n3); */
-
-/*  var a = true;
-var b = false;
-var c = true; */
-
-/*console.log( a && b);
-console.log(b && c );
-console.log(a && c );
-console.log(a && b && c );
-
-console.log( a || b);
-console.log(b || c );
-console.log(a || c );
-console.log(a || b || c ); */
-
-/* console.log(!a);
-console.log(!b);
-console.log(!c); */
-
-/* var nome = "LaÍS"
-const idade = 23 
-var outroNome = "    testetesteteste   "
-var anoAtual = 2023
-
-console.log( nome, idade, (idade >= 18) )
-console.log("idade em 2050: " + (idade + (2050 - anoAtual)));
-
-console.log(nome.length); /* conta caracteres
-console.log(nome.toLowerCase()); /* formata o texto pra minúsculo  -
-console.log(outroNome.trim()); /* tira espaço de fora da frase */
-
-/* const frase = "hoje eu comi cenoura"; /*fala se inclui tal palavra na frase -
-console.log(frase.includes("cenoura"));  
-console.log(frase.includes("batata"));  */
-
-/* const frase = "hoje eu comi cenoura, adoro cenoura";
-const novaFrase = frase.replaceAll("cenoura", "batata"); /* muda as palavras -
-console.log(novaFrase);
-  */
+    default:
+        console.log("desconhecido")
+        break;
+}
 
 
-const listaDeCompras = ["batata", "alface", "queijo"];
-const listaDeNumeroMega = [2, 13, 26, 35, 74, 60];
+var pokemon = "Squirtle"
 
-listaDeCompras.push("cenoura");
-listaDeCompras[4] = "papel";
-listaDeCompras[listaDeCompras.length] = "cebola";
-listaDeCompras[1] = "tomate";
-console.log(listaDeCompras);
+switch (pokemon) {
+    case "Bulbasauro":
+        console.log("Planta e Veneno")
+        break;
+    case "Charmander":
+        console.log("Fogo")
+        break;
+    case "Squirtle":
+        console.log("Água")
+        break;
 
-/*listaDeCompras.pop()*/
+    default:
+        console.log("desconhecido")
+        break;
+}
+ */
 
-listaDeCompras.splice(3, 2)
+/* var lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-console.log(listaDeCompras); /* fala todos da lista - */
-console.log(listaDeCompras[0]); /* fala só o coisa da lista na posição - */
-console.log(listaDeCompras[1]);
-console.log(listaDeCompras[2]);
-console.log(listaDeCompras[3]);
-console.log(listaDeCompras.length); /* fala quantas posições tem - */ 
+lista.forEach(element => {
+    console.log(element);
+});
+
+for (const element of lista) {
+    console.log(element);
+}
+
+for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    console.log(element);
+} */
+
+/* var i = 0;
+while (i < 10) {
+    console.log(i);
+    i = i +1;
+} */
 
 
 
+/* function calcularArea( altura = 10, largura = 20) {
+    var area = altura * largura;
+    return(area);
+}
+
+function somaAreas(largura1, largura2, altura1, altura2) {
+    var area1 = calcularArea(largura1, altura1);
+    var area2 = calcularArea(largura2, altura2);
+    return(area1 + area2);
+}
+
+console.log(somaAreas(10,20,30,40));
+ */
+
+
+/* function imprimeNomes(nome) {
+    console.log(nome);
+}
+
+imprimeNomes("nome1");
+imprimeNomes("nome2");
+imprimeNomes("nome3");
+
+function imprimeNomesComReturn(nome) {
+    return nome;
+}
+
+console.log(imprimeNomesComReturn("nome1"))
+console.log(imprimeNomesComReturn("nome2"))
+console.log(imprimeNomesComReturn("nome3")) */
+
+
+
+/* function recebeArray(lista = []) {
+    var arrayFinal = [];
+    arrayFinal[0] = lista[lista.length -1];
+    arrayFinal[1] = lista [0];
+    console.log(arrayFinal);
+}
+var lista = [1,2,3,4,5,6,7,8,9];
+recebeArray(lista); */
+
+
+
+
+
+
+// Declaração de variáveis 
+const header = document.createElement("header");
+const content = document.createElement("main");
+const footer = document.createElement("footer");
+
+// Adiciona conteúdo ao cabeçalho 
+header.innerHTML = `
+    <h1>cabeçalho</h1>
+    <p>Este é o cabeçalho da página</p>
+ `;
+
+ // Adiciona conteúdo ao conteúdo 
+ content.innerHTML = `
+    <h2>conteúdo</h2>
+    <p>Este é o conteúdo da pagina.</p>
+ `;
+ // Adiciona conteúdo ao rodapé
+ footer.innerHTML = `
+    <p>Rodapé</p>
+`;
+
+// Adiciona os elementos à página
+document.body.appendChild(header);
+document.body.appendChild(content);
+document.body.appendChild(footer);
+
+//Aplica estilos ao cabeçalho 
+header.style.backgroundColor ="#ccc";
+header.style.padding = "10px";
+
+//Aplica estilos ao conteúdo 
+content.style.backgroundColor = "#fff";
+content.style.padding = "20px";
+
+//Aplica estilo ao rodapé
+footer.style.backgroundColor = "#ccc";
+footer.style.padding = "10px";
+
+
+const container = document.querySelector("main");
+const linhas = [];
+const cores = ["#fff", "#ccc"];
+
+for (let i = 0; i < 10; i++) {
+    const linha = document.createElement("div");
+    linha.classList.add("linha");
+    linha.innerHTML = `
+    <h3>Linha ${i + 1}</h3> 
+    <p>Este é o conteúdo da linha ${i + 1}.</p>`
+    
+}
 
 
---
+++ old_index.js
@@ -0,0 +1,173 @@
+ /*console.log(prompt("Qual é o seu nome?")); - "imprime" -
+
+var variavel = 100; - muda -
+const variavelConstante = 200; - constante -
+
+variavel = 400;
+variavelConstante = 600;
+
+const corDeFundo = #fefefe */
+
+/*const nome = "Laura";
+const idade = 18;
+
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos");*/
+
+/*Numbers - representa numero*/
+/*const idade = 23;
+const altura = 1.79;
+const temperatura = -20;
+
+/*Strings - representa texto
+const nome = "Yuzo";
+var idade = "23";
+
+var souUmBoolean = true;
+console.log(souUmBoolean);
+souUmBoolean = false;
+console.log(souUmBoolean);*/
+
+/* const primeiroNome = "Laura";
+const sobrenome = "Lange";
+const idade = 19;
+const souEstudante = "true";
+
+console.log("nome:", primeiroNome, " Sobrenome:", sobrenome, " idade:", idade, " anos", " sou estudante?", souEstudante,);
+
+console.log("nome: " + primeiroNome + " Sobrenome: " + sobrenome + " idade: " + idade + " anos" + " é estudante?: "+ souEstudante);
+
+var todoTexto = "nome: " + primeiroNome + "  Sobrenome: " + sobrenome + "  idade: " + idade + " anos" + "  é estudante?: "+ souEstudante;
+console.log(todoTexto);
+ 
+
+console.log(typeof(primeiroNome));
+console.log(typeof(sobrenome));
+console.log(typeof(idade));
+console.log(typeof(souEstudante)); */
+
+/* - Numero pra texto -
+const idadeNumero = 23;
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero);
+console.log(typeof idadeTexto);*/
+
+
+/* /* - Texto pra Numero - 
+const idadeTexto = "23";
+const idadeNumero = Number(idadeTexto);
+
+console.log(typeof idadeTexto);
+console.log(typeof idadeNumero) */
+
+/* - matematica -
+ var n1 = 10;
+var n2 = 5;
+var n3 = 3;
+
+var soma, subtracao, divisao, multiplicacao;
+
+soma = n1 + n2; 
+subtracao = n1 - n2;
+divisao = n1 / n2;
+multiplicacao = n1 * n2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(divisao);
+console.log(multiplicacao); */
+
+/* dividido por 2 = 0 par 
+var numero = 6 
+console.log(numero % 2); */
+
+/* /* comparador - 
+=== igual
+!== diferente 
+>= > 
+<= < 
+|| - OU
+&& - E 
+! - negação */
+
+/* var n1 = 2;
+var n2 = 564;
+var n3 = 2;
+var n4 = 3; 
+
+/* console.log(n1 === n2);
+console.log(n1 === n3); 
+
+console.log(n1 !== n2);
+console.log(n1 !== n3);  
+
+console.log(n1 > n3); 
+console.log(n1 >= n3); 
+
+console.log(n1 === n2 && n1 === n3);
+console.log(n1 === n2 || n1 === n3); */
+
+/*  var a = true;
+var b = false;
+var c = true; */
+
+/*console.log( a && b);
+console.log(b && c );
+console.log(a && c );
+console.log(a && b && c );
+
+console.log( a || b);
+console.log(b || c );
+console.log(a || c );
+console.log(a || b || c ); */
+
+/* console.log(!a);
+console.log(!b);
+console.log(!c); */
+
+/* var nome = "LaÍS"
+const idade = 23 
+var outroNome = "    testetesteteste   "
+var anoAtual = 2023
+
+console.log( nome, idade, (idade >= 18) )
+console.log("idade em 2050: " + (idade + (2050 - anoAtual)));
+
+console.log(nome.length); /* conta caracteres
+console.log(nome.toLowerCase()); /* formata o texto pra minúsculo  -
+console.log(outroNome.trim()); /* tira espaço de fora da frase */
+
+/* const frase = "hoje eu comi cenoura"; /*
+console.log(frase.includes("cenoura"));  fala se inclui tal palavra na frase -
+console.log(frase.includes("batata"));  */
+
+/* const frase = "hoje eu comi cenoura, adoro cenoura";
+const novaFrase = frase.replaceAll("cenoura", "batata"); /* muda as palavras -
+console.log(novaFrase);
+  */
+
+
+/* const listaDeCompras = ["batata", "alface", "queijo"];
+const listaDeNumeroMega = [2, 13, 26, 35, 74, 60];
+
+listaDeCompras.push("cenoura");
+listaDeCompras[4] = "papel";
+listaDeCompras[listaDeCompras.length] = "cebola";
+listaDeCompras[1] = "tomate";
+console.log(listaDeCompras);
+
+/*listaDeCompras.pop()
+
+listaDeCompras.splice(3, 2)
+
+console.log(listaDeCompras); /* fala todos da lista - 
+console.log(listaDeCompras[0]); /* fala só o coisa da lista na posição - 
+console.log(listaDeCompras[1]);
+console.log(listaDeCompras[2]);
+console.log(listaDeCompras[3]);
+console.log(listaDeCompras.length); /* fala quantas posições tem - */ 
+ 
+
+
+
+
